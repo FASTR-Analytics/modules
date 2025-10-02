407c9dfbdebb108e2c3a1ee0f076a3320affdcc5
clairecda <claire.boulange@uwa.edu.au>
2025-10-02 13:57:04 +0800

    admin area 4 option

--- 03_module_service_utilization.R
+++ 03_module_service_utilization.R
@@ -20,7 +20,8 @@ RUN_DISTRICT_MODEL <- FALSE             # Set to TRUE to run regressions at the 
 RUN_ADMIN_AREA_4_ANALYSIS <- FALSE      # Set to TRUE to run finest-level analysis (admin_area_4)
                                        # Warning: This can be very slow for large datasets
 
-CONTROL_CHART_LEVEL <- "admin_area_2"  # Options: "admin_area_2" or "admin_area_3" default should be admin_area_2!
+CONTROL_CHART_LEVEL <- "admin_area_2"  # Options: "admin_area_2" or "admin_area_3" OR "admin_area_4"
+                                       # default should be admin_area_2!
                                        # in new Nigeria instance >>> admin_area_2 = Geopolitical Regions 
                                        # & admin_area_3 = State level
 
